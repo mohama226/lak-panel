e60b05ed7c1d72766d3960c0108e9a02d424d174
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-05 01:16:55 +0330

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,18 +1,12 @@
 import uvicorn
 
-from app.db.database import Base
-from app.db.database import engine
+from app.core.config import settings
 
-# Import models
-import app.models.admin
-import app.models.server
-
-Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
-        host="0.0.0.0",
-        port=8000,
+        host=settings.APP_HOST,
+        port=settings.APP_PORT,
         reload=False
     )
