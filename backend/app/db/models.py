4fa832810ccf46cbceca7b395d23bda18d4de53b
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-04 11:35:19 +0330

    Create models.py

---
+++ backend/app/db/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from app.db.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    expires_at = Column(DateTime, nullable=True)
