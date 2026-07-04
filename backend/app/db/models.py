c275cb42d6f7267327350fd970cabf88b87b03aa
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-05 00:29:10 +0330

    Update models.py

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,59 +1,7 @@
-from sqlalchemy import Column
-from sqlalchemy import Integer
-from sqlalchemy import String
+from app.models.admin import Admin
+from app.models.server import Server
 
-from .database import Base
-
-
-class Server(Base):
-
-    __tablename__ = "servers"
-
-    id = Column(
-        Integer,
-        primary_key=True,
-        index=True
-    )
-
-    name = Column(
-        String,
-        nullable=False
-    )
-
-    host = Column(
-        String,
-        nullable=False
-    )
-
-    ssh_port = Column(
-        Integer,
-        default=22
-    )
-
-    ssh_user = Column(
-        String,
-        nullable=False
-    )
-
-    auth_type = Column(
-        String,
-        default="password"
-    )
-
-    ssh_password = Column(
-        String
-    )
-
-    ssh_private_key = Column(
-        String
-    )
-
-    vpn_type = Column(
-        String,
-        default="ocserv"
-    )
-
-    status = Column(
-        String,
-        default="offline"
-    )
+__all__ = [
+    "Admin",
+    "Server",
+]
