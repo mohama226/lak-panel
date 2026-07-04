4a9660e9283737fe640845d394955c51c856f605
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-05 00:28:25 +0330

    Update admin.py

--- backend/app/models/admin.py
+++ backend/app/models/admin.py
@@ -1,1 +1,40 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import DateTime
 
+from datetime import datetime
+
+from app.db.database import Base
+
+
+class Admin(Base):
+
+    __tablename__ = "admins"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    username = Column(
+        String,
+        unique=True,
+        nullable=False
+    )
+
+    password = Column(
+        String,
+        nullable=False
+    )
+
+    role = Column(
+        String,
+        default="superadmin"
+    )
+
+    created_at = Column(
+        DateTime,
+        default=datetime.utcnow
+    )
