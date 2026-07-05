5030d780b53870c7ae9a7a2989aa53f4f1ab80cc
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-05 08:56:31 +0330

    Update admins.py

--- backend/app/routers/admins.py
+++ backend/app/routers/admins.py
@@ -1,32 +1,63 @@
 from fastapi import APIRouter
 from fastapi import Request
+from fastapi import Form
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 
-templates = Jinja2Templates(
-    directory="app/templates"
-)
+templates = Jinja2Templates(directory="app/templates")
+
+admins = []
 
 
 @router.get("/admins")
-async def admins_page(request: Request):
-
-    roles = [
-        "Super Admin",
-        "Admin",
-        "Operator",
-        "Support",
-        "Viewer",
-    ]
-
-    admins = []
+async def admin_list(request: Request):
 
     return templates.TemplateResponse(
         "admins/list.html",
         {
             "request": request,
-            "roles": roles,
             "admins": admins,
         },
+    )
+
+
+@router.get("/admins/create")
+async def create_admin_page(request: Request):
+
+    return templates.TemplateResponse(
+        "admins/create.html",
+        {
+            "request": request,
+        },
+    )
+
+
+@router.post("/admins/create")
+async def create_admin(
+
+    username: str = Form(...),
+
+    password: str = Form(...),
+
+    fullname: str = Form(...),
+
+    role: str = Form(...),
+
+):
+
+    admins.append(
+        {
+            "id": len(admins) + 1,
+            "username": username,
+            "fullname": fullname,
+            "role": role,
+            "active": True,
+        }
+    )
+
+    return RedirectResponse(
+        "/admins",
+        status_code=302,
     )
