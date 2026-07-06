007ff359e6229f17824fb96c33cfb5522ad22642
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-06 19:31:24 +0330

    Create auth.py

---
+++ backend/app/core/auth.py
@@ -0,0 +1,25 @@
+from fastapi import Cookie
+from fastapi import HTTPException
+
+from app.db.database import SessionLocal
+from app.db.models import Admin
+
+
+def require_login(
+    lak_admin: str | None = Cookie(default=None),
+):
+    if not lak_admin:
+        raise HTTPException(status_code=401)
+
+    db = SessionLocal()
+
+    admin = (
+        db.query(Admin)
+        .filter(Admin.id == int(lak_admin))
+        .first()
+    )
+
+    if not admin:
+        raise HTTPException(status_code=401)
+
+    return admin
