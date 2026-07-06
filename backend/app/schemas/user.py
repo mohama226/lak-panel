c16ef97dac934e5f229ba841a19020a31de95844
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-06 15:21:25 +0330

    Update user.py

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -5,33 +5,59 @@ from pydantic import BaseModel, Field
 
 
 class UserCreate(BaseModel):
-    username: str = Field(min_length=3, max_length=100)
-    password: str = Field(min_length=4)
+
+    username: str = Field(
+        min_length=3,
+        max_length=100,
+    )
+
+    password: str = Field(
+        min_length=4,
+    )
+
     expire: Optional[datetime] = None
+
     traffic: int = 0
-    group_id: Optional[int] = None
-    server_id: Optional[int] = None
 
-
-class UserUpdate(BaseModel):
-    expire: Optional[datetime] = None
-    traffic: Optional[int] = None
-    enabled: Optional[bool] = None
     group_id: Optional[int] = None
+
     server_id: Optional[int] = None
 
 
 class UserPassword(BaseModel):
-    password: str = Field(min_length=4)
+
+    password: str = Field(
+        min_length=4,
+    )
+
+
+class UserUpdate(BaseModel):
+
+    expire: Optional[datetime] = None
+
+    traffic: Optional[int] = None
+
+    enabled: Optional[bool] = None
+
+    group_id: Optional[int] = None
+
+    server_id: Optional[int] = None
 
 
 class UserOut(BaseModel):
+
     id: int
+
     username: str
+
     expire: Optional[datetime]
+
     traffic: int
+
     enabled: bool
+
     group_id: Optional[int]
+
     server_id: Optional[int]
 
     class Config:
