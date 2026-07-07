7be164c81d426e3a60d3450b9f880ea0860bff04
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-08 01:25:45 +0330

    Update database.py

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -4,23 +4,34 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
+
 engine = create_engine(
     settings.DATABASE_URL,
     connect_args={"check_same_thread": False},
+    pool_pre_ping=True,
+    pool_recycle=300,
 )
 
+
 SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
     bind=engine,
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False,
 )
+
 
 Base = declarative_base()
 
 
 def get_db():
+
     db = SessionLocal()
+
     try:
+
         yield db
+
     finally:
+
         db.close()
