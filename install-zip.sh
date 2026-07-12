c4d40d4b02c1a42f758f0309271d31190996dc75
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-12 21:38:30 +0330

    Update install-zip.sh

--- install-zip.sh
+++ install-zip.sh
@@ -8,35 +8,22 @@ echo "======================================"
 echo "       LAK PANEL ZIP INSTALLER"
 echo "======================================"
 
-
+rm -rf /tmp/lak-panel-main
 rm -rf $BASE
-
-mkdir -p /opt
-
 
 cd /opt
 
-
-echo "Downloading LAK Panel..."
-
-wget -O lak-panel.zip \
-https://github.com/mohama226/lak-panel/archive/refs/heads/main.zip
-
-
-echo "Extracting..."
+wget -O lak-panel.zip https://github.com/mohama226/lak-panel/archive/refs/heads/main.zip
 
 unzip -o lak-panel.zip
 
-
-mv lak-panel-main lak-panel
-
-
-rm lak-panel.zip
+mv lak-panel-main $BASE
 
 
-echo ""
 echo "Project installed:"
 echo "$BASE"
 
+
+chmod +x $BASE/install.sh
 
 bash $BASE/install.sh
