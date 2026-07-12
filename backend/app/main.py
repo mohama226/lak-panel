4d524bfd1d82ee0a9f87375cf201845fa38c77c4
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-12 22:53:32 +0330

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,26 @@
 from fastapi import FastAPI
 
+from .database import Base, engine
+
+from .models import *
+
+
+Base.metadata.create_all(
+    bind=engine
+)
+
+
 app = FastAPI(
     title="LAK Panel",
     version="0.1.0"
 )
 
 
+
 @app.get("/")
 def home():
+
     return {
-        "status": "running",
-        "panel": "LAK Panel"
+        "status":"running",
+        "panel":"LAK Panel"
     }
