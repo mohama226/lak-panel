7fa635eb00ee4d7b439d823c2b2c911d16205004
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-13 17:43:54 +0330

    Create install.sh

---
+++ install/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source "$(dirname "$0")/variables.sh"
+
+source "$(dirname "$0")/functions.sh"
+
+source "$(dirname "$0")/logo.sh"
+
+check_root
+
+check_os
+
+create_directories
+
+bash "$(dirname "$0")/postgres.sh"
+
+bash "$(dirname "$0")/nginx.sh"
+
+bash "$(dirname "$0")/service.sh"
+
+bash "$(dirname "$0")/ssl.sh"
+
+bash "$(dirname "$0")/firewall.sh"
+
+bash "$(dirname "$0")/ocserv.sh"
+
+green "Installation Finished."
