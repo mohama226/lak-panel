18e68f2d1ab4582a0bf7ee33e381a946ccead3aa
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-13 18:15:51 +0330

    Update functions.sh

--- install/functions.sh
+++ install/functions.sh
@@ -1,43 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 green() {
-
     echo -e "\e[32m$1\e[0m"
-
 }
 
 red() {
-
     echo -e "\e[31m$1\e[0m"
-
 }
 
 yellow() {
-
     echo -e "\e[33m$1\e[0m"
-
 }
 
 blue() {
-
     echo -e "\e[36m$1\e[0m"
-
 }
 
 error_exit() {
-
     red "$1"
-
     exit 1
-
 }
 
 check_root() {
 
-    if [[ $EUID -ne 0 ]]; then
-
+    if [ "$EUID" -ne 0 ]; then
         error_exit "Please run installer as root."
-
     fi
 
 }
@@ -46,10 +33,8 @@ check_os() {
 
     source /etc/os-release
 
-    if [[ "$ID" != "ubuntu" ]]; then
-
+    if [ "$ID" != "ubuntu" ]; then
         error_exit "Only Ubuntu is supported."
-
     fi
 
 }
@@ -64,8 +49,26 @@ create_directories() {
 
     mkdir -p "$BACKUP_DIR"
 
+    mkdir -p "$UPLOAD_DIR"
+
     mkdir -p "$TMP_DIR"
 
-    mkdir -p "$UPLOAD_DIR"
+}
+
+install_packages() {
+
+    blue "Installing required packages..."
+
+    apt update
+
+    apt install -y \
+        git \
+        curl \
+        wget \
+        unzip \
+        python3 \
+        python3-pip \
+        python3-venv \
+        build-essential
 
 }
