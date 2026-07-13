029420d629d7fa50f02189ad1047f5df3619614b
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-13 18:37:02 +0330

    Update postgres.sh

--- install/postgres.sh
+++ install/postgres.sh
@@ -1,9 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-blue "Installing PostgreSQL ..."
-
-apt update
+blue "Installing PostgreSQL..."
 
 apt install -y postgresql postgresql-contrib
 
-green "PostgreSQL Installed."
+systemctl enable postgresql
+
+systemctl start postgresql
+
+
+blue "Creating Database..."
+
+
+sudo -u postgres psql <<EOF
+
+CREATE USER ${POSTGRES_USER} WITH PASSWORD '${POSTGRES_PASSWORD}';
+
+CREATE DATABASE ${POSTGRES_DB};
+
+GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_USER};
+
+EOF
+
+
+green "PostgreSQL Ready."
