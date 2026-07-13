175b5a97c362e8366d124b702d0027d7fac22dde
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-14 01:14:25 +0330

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DB_USER=os.getenv("DB_USER")
-DB_PASS=os.getenv("DB_PASS")
-DB_NAME=os.getenv("DB_NAME")
-DB_HOST="localhost"
-
-SQLALCHEMY_DATABASE_URI=f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
-SQLALCHEMY_TRACK_MODIFICATIONS=False
