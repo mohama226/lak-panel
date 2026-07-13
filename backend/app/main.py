5f0c15cf48ce3929dd06aedf70796c3c02f2c8c3
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-13 17:15:12 +0330

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,28 @@
 from fastapi import FastAPI
 
+from app.lifespan import lifespan
+
 app = FastAPI(
+
     title="L-Panel",
-    version="1.0.0"
+
+    version="1.0.0",
+
+    lifespan=lifespan
+
 )
 
 
 @app.get("/")
+
 async def home():
 
     return {
+
         "project": "L-Panel",
+
         "status": "running",
+
         "version": "1.0.0"
+
     }
