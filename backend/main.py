e65476578b666c499b7d4e2b036ae36838c3f692
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-13 20:18:25 +0330

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.database import Base, engine
 
 from app.admin import router as admin_router
+from app.users import router as users_router
 
 
 Base.metadata.create_all(bind=engine)
@@ -18,10 +19,13 @@ app.include_router(
     admin_router
 )
 
+app.include_router(
+    users_router
+)
+
 
 @app.get("/")
 def home():
-
     return {
         "name": "L-Panel",
         "status": "running"
