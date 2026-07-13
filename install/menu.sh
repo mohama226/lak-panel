d4c6a6d90789d3be30d5cd33e2b4dacea284e6e2
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-13 18:06:31 +0330

    Update menu.sh

--- install/menu.sh
+++ install/menu.sh
@@ -2,15 +2,24 @@
 
 clear
 
-echo "====================================="
-echo "          L-PANEL CLI"
-echo "====================================="
+echo "======================================"
+echo "            L-PANEL"
+echo "======================================"
+
 echo
+
 echo "1) Start Panel"
+
 echo "2) Stop Panel"
+
 echo "3) Restart Panel"
+
 echo "4) Status"
-echo "5) Exit"
+
+echo "5) Update"
+
+echo "6) Exit"
+
 echo
 
 read -rp "Select: " choice
@@ -18,27 +27,45 @@ read -rp "Select: " choice
 case "$choice" in
 
 1)
-    systemctl start l-panel
-    ;;
+
+systemctl start l-panel
+
+;;
 
 2)
-    systemctl stop l-panel
-    ;;
+
+systemctl stop l-panel
+
+;;
 
 3)
-    systemctl restart l-panel
-    ;;
+
+systemctl restart l-panel
+
+;;
 
 4)
-    systemctl status l-panel
-    ;;
+
+systemctl status l-panel
+
+;;
 
 5)
-    exit 0
-    ;;
+
+bash /opt/l-panel/install/update.sh
+
+;;
+
+6)
+
+exit
+
+;;
 
 *)
-    echo "Invalid Option"
-    ;;
+
+echo "Invalid Option"
+
+;;
 
 esac
