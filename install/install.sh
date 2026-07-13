29a388f978af33d6c12c91ee88aaca6bf96ee097
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-13 19:20:52 +0330

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -7,30 +7,25 @@ REPO_URL="https://github.com/mohama226/l-panel.git"
 BRANCH="main"
 
 echo "========================================"
-echo "          L-PANEL INSTALLER"
+echo "        L-PANEL INSTALLER"
 echo "========================================"
 
 if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root."
+    echo "Please run installer as root."
     exit 1
 fi
 
-echo "Updating packages..."
 apt update
 
-echo "Installing Git..."
 apt install -y git
 
 if [ -d "$INSTALL_DIR" ]; then
     rm -rf "$INSTALL_DIR"
 fi
 
-echo "Downloading L-Panel..."
-git clone -b "$BRANCH" "$REPO_URL" "$INSTALL_DIR"
+git clone --depth=1 -b "$BRANCH" "$REPO_URL" "$INSTALL_DIR"
 
 chmod -R +x "$INSTALL_DIR/install"
 chmod -R +x "$INSTALL_DIR/cli"
-
-echo "Starting Installer..."
 
 exec bash "$INSTALL_DIR/install/setup.sh"
