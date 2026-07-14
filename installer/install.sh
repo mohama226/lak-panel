e7dff12f2d47fa3074047ff0bf2b45a12d4be5bd
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-14 21:41:37 +0330

    Update install.sh

--- installer/install.sh
+++ installer/install.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+set -e
+
+APP_DIR="/opt/l-panel"
 
 apt update
+apt install -y git python3 python3-pip python3-venv
 
-apt install -y python3 python3-pip python3-venv
+rm -rf "$APP_DIR"
+
+git clone https://github.com/mohama226/l-panel.git "$APP_DIR"
+
+cd "$APP_DIR"
 
 python3 -m venv venv
-
 source venv/bin/activate
 
+pip install --upgrade pip
 pip install -r requirements.txt
 
 python3 run.py
