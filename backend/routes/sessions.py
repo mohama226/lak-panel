d4c29f393c9fdf825ebc041671b2eb4e38aaf076
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-15 17:57:09 +0330

    Update sessions.py

--- backend/routes/sessions.py
+++ backend/routes/sessions.py
@@ -1,1 +1,12 @@
-#
+from flask import Blueprint
+
+sessions_bp = Blueprint(
+    "sessions",
+    __name__,
+    url_prefix="/sessions"
+)
+
+@sessions_bp.route("/")
+def sessions():
+
+    return "Sessions"
