bf62275eaf54785e9733abaee21c03533fd17e4f
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-15 21:40:03 +0330

    Update 001_add_user_columns.sql

--- backend/database/migrations/001_add_user_columns.sql
+++ backend/database/migrations/001_add_user_columns.sql
@@ -12,18 +12,3 @@ ADD COLUMN IF NOT EXISTS created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
 
 ALTER TABLE users
 ADD COLUMN IF NOT EXISTS server_id INTEGER;
-
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1
-        FROM information_schema.table_constraints
-        WHERE constraint_name = 'users_server_id_fkey'
-    )
-    THEN
-        ALTER TABLE users
-        ADD CONSTRAINT users_server_id_fkey
-        FOREIGN KEY (server_id)
-        REFERENCES servers(id);
-    END IF;
-END $$;
