1ddeb3779c4a68312d07aac80506514008f041e0
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-15 22:03:04 +0330

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,45 +1,63 @@
 from flask import Flask
-
-from backend.config import Config
 from backend.extensions import db
+from backend.config import Config
 
 
 def create_app():
+
     app = Flask(__name__)
 
-    # Config
     app.config.from_object(Config)
 
-    # Database
+
     db.init_app(app)
 
-    # Import models
-    from backend.models.user import User
-    from backend.models.server import Server
-    from backend.models.session import Session
 
-    # Create tables + update users table
+    from backend.models import (
+        User,
+        Server,
+        Session
+    )
+
+
     with app.app_context():
 
         db.create_all()
 
-        from backend.database.update_users import update_users_table
-        update_users_table()
 
-    # ==========================
-    # Register Blueprints
-    # ==========================
+        from backend.database.migrations import migrate_database
 
-    from backend.routes.dashboard import dashboard_bp
-    from backend.routes.auth import auth_bp
-    from backend.routes.users import users_bp
-    from backend.routes.servers import servers_bp
-    from backend.routes.sessions import sessions_bp
+        migrate_database()
+
+
+
+    from backend.routes import (
+        dashboard_bp,
+        users_bp,
+        servers_bp,
+        sessions_bp,
+        auth_bp
+    )
+
 
     app.register_blueprint(dashboard_bp)
-    app.register_blueprint(auth_bp)
     app.register_blueprint(users_bp)
     app.register_blueprint(servers_bp)
     app.register_blueprint(sessions_bp)
+    app.register_blueprint(auth_bp)
+
+
+
+    from backend.api import (
+        users_api,
+        servers_api,
+        sessions_api
+    )
+
+
+    app.register_blueprint(users_api)
+    app.register_blueprint(servers_api)
+    app.register_blueprint(sessions_api)
+
 
     return app
