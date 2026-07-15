082e43c65f78e8d93de67c8b1503604ae11fc418
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-15 07:54:23 +0330

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask
+from backend.extensions import db
+from backend.config import Config
+
 
 def create_app():
+
     app = Flask(__name__)
 
-    @app.route("/")
-    def index():
-        return render_template("index.html")
+    app.config.from_object(Config)
+
+
+    db.init_app(app)
+
+
+    from backend.models import (
+        User,
+        Server,
+        Session
+    )
+
+
+    with app.app_context():
+        db.create_all()
+
 
     return app
