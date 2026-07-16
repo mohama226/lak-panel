3c5fac9a002703eeb89fec8ca1a51a3672fbe43c
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-16 21:48:36 +0330

    Create deps.sh

---
+++ install/deps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "[+] Installing system dependencies..."
+
+dnf install -y epel-release
+dnf install -y python3 python3-pip python3-devel gcc gcc-c++ make \
+               postgresql postgresql-server postgresql-devel \
+               ocserv git
+
+echo "[+] Dependencies installed."
