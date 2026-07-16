c8c65c2a8a6bf346e47faa8e35331cd5c898af6c
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-17 01:01:00 +0330

    Update install.sh

--- installer/install.sh
+++ installer/install.sh
@@ -1,5 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+set -Eeuo pipefail
+
+INSTALL_DIR="/opt/l-panel"
+
+echo
 echo "Installing L-Panel..."
+echo
 
-echo "Coming Soon"
+mkdir -p "$INSTALL_DIR"
+
+cp -r ./* "$INSTALL_DIR"
+
+chmod +x "$INSTALL_DIR/cli/l-panel"
+
+ln -sf "$INSTALL_DIR/cli/l-panel" /usr/local/bin/l-panel
+
+echo
+echo "Installation Complete."
+echo
+
+echo "Run:"
+echo
+
+echo "l-panel"
