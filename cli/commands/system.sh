2abaf236732de17402dda59270f2f1d427dd406a
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-17 20:53:07 +0330

    Delete cli/commands/system.sh

--- cli/commands/system.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-
-source "$SCRIPT_DIR/lib/colors.sh"
-source "$SCRIPT_DIR/lib/common.sh"
-source "$SCRIPT_DIR/lib/version.sh"
