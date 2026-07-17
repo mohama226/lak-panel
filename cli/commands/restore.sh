48fe3a3b223f06d22cde68e9fbef6ff2854ef41f
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-17 15:47:04 +0330

    Update restore.sh

--- cli/commands/restore.sh
+++ cli/commands/restore.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/env bash
 
+SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+CLI_DIR="$(dirname "$SCRIPT_DIR")"
+
+source "$CLI_DIR/lib/colors.sh"
+source "$CLI_DIR/lib/common.sh"
+
+title
+
+warn "Coming Soon..."
+
+pause
