531dcaae3dda8da2174e2566d78f0fcf5f203ed5
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-23 11:21:28 +0330

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,41 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 
-import { QueryClient } from "@tanstack/react-query";
-import { QueryClientProvider } from "@tanstack/react-query";
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-import "./styles/globals.css";
+import "./styles/global.css";
 
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            retry: 1,
-            refetchOnWindowFocus: false,
-            staleTime: 60000
-        },
-        mutations: {
-            retry: 0
-        }
-    }
-});
 
-ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-).render(
-    <React.StrictMode>
+ReactDOM
+    .createRoot(
+        document.getElementById("root")!
+    )
+    .render(
 
-        <BrowserRouter>
+        <React.StrictMode>
 
-            <QueryClientProvider client={queryClient}>
+            <App />
 
-                <App />
+        </React.StrictMode>
 
-            </QueryClientProvider>
-
-        </BrowserRouter>
-
-    </React.StrictMode>
-);
+    );
