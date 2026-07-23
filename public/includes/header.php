a3de1a8f75c47f6617b39502c68a2b55cfcc4d48
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-23 20:39:50 +0330

    Update header.php

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,18 +1,25 @@
+<?php
+
+if(session_status()===PHP_SESSION_NONE){
+
+session_start();
+
+}
+
+?>
+
 <!DOCTYPE html>
+
 <html lang="fa" dir="rtl">
 
 <head>
 
 <meta charset="UTF-8">
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
 <title>L-PANEL</title>
 
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" rel="stylesheet">
-
-<link href="assets/css/style.css" rel="stylesheet">
+<link rel="stylesheet" href="../assets/css/style.css">
 
 
 </head>
