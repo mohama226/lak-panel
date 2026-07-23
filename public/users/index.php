c2ed44410836529775c91a3f094c0e8e41e97dc8
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-23 20:27:49 +0330

    Create index.php

---
+++ public/users/index.php
@@ -0,0 +1,141 @@
+<?php
+
+require "../../app/database.php";
+
+session_start();
+
+
+$error="";
+
+
+if($_POST){
+
+
+$username=$_POST['username'];
+
+$password=$_POST['password'];
+
+
+
+$stmt=$db->prepare(
+"SELECT * FROM users WHERE username=?"
+);
+
+
+$stmt->execute([$username]);
+
+
+$user=$stmt->fetch();
+
+
+
+if($user && password_verify($password,$user['password'])){
+
+
+$_SESSION['vpn_user']=$user['username'];
+
+$_SESSION['vpn_id']=$user['id'];
+
+
+header("Location: dashboard.php");
+
+exit;
+
+
+}else{
+
+
+$error="اطلاعات ورود اشتباه است";
+
+
+}
+
+
+}
+
+?>
+
+
+<!DOCTYPE html>
+
+<html lang="fa" dir="rtl">
+
+
+<head>
+
+<meta charset="UTF-8">
+
+<title>
+User Login
+</title>
+
+
+<link rel="stylesheet" href="../assets/css/login.css">
+
+
+</head>
+
+
+<body>
+
+
+<div class="login-box">
+
+
+<div class="logo">
+
+<h1>
+L-PANEL
+</h1>
+
+<span>
+VPN User Panel
+</span>
+
+
+</div>
+
+
+
+<?php if($error): ?>
+
+<div class="error">
+
+<?=$error?>
+
+</div>
+
+<?php endif; ?>
+
+
+
+<form method="post">
+
+
+<input class="form-control"
+name="username"
+placeholder="نام کاربری VPN">
+
+
+<input class="form-control"
+type="password"
+name="password"
+placeholder="رمز عبور">
+
+
+<button class="login-btn">
+
+ورود
+
+</button>
+
+
+</form>
+
+
+</div>
+
+
+</body>
+
+</html>
