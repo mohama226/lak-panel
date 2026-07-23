98047eac677592b36aa70a7c45b8c42c0bc014d4
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-23 22:56:42 +0330

    Update header.php

--- public/includes/header.php
+++ public/includes/header.php
@@ -13,8 +13,10 @@ session_start();
 <meta charset="UTF-8">
 
 <title>
-L-PANEL
+L-PANEL | مدیریت VPN
 </title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
 <link rel="stylesheet" href="/assets/css/style.css">
