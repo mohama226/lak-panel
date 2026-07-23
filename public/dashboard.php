e3f0573bbb6bd9a956b4eeb994e0a599ca07af83
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-23 20:14:16 +0330

    Update dashboard.php

--- public/dashboard.php
+++ public/dashboard.php
@@ -4,12 +4,79 @@ require "../app/auth.php";
 
 checkLogin();
 
+include "includes/header.php";
+
+include "includes/sidebar.php";
+
 ?>
 
-<h1>
-Welcome <?php echo $_SESSION['admin']; ?>
-</h1>
 
-<a href="logout.php">
-Logout
-</a>
+<h2>
+سلام <?=$_SESSION['admin']?>
+</h2>
+
+
+<div class="row mt-4">
+
+
+<div class="col-md-4">
+
+<div class="card p-4 shadow">
+
+<h5>
+کاربران
+</h5>
+
+<h2>
+0
+</h2>
+
+</div>
+
+</div>
+
+
+
+<div class="col-md-4">
+
+<div class="card p-4 shadow">
+
+<h5>
+اتصالات فعال
+</h5>
+
+<h2>
+0
+</h2>
+
+</div>
+
+</div>
+
+
+
+<div class="col-md-4">
+
+<div class="card p-4 shadow">
+
+<h5>
+سرورها
+</h5>
+
+<h2>
+1
+</h2>
+
+</div>
+
+</div>
+
+
+</div>
+
+
+<?php
+
+include "includes/footer.php";
+
+?>
