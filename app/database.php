18d52453520f248ab123c87a93ebee54c049790a
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-23 20:32:16 +0330

    Update database.php

--- app/database.php
+++ app/database.php
@@ -1,14 +1,25 @@
 <?php
 
+
 $db = new PDO(
+
 "mysql:host=localhost;dbname=lpanel;charset=utf8mb4",
+
 "lpanel",
+
 "lpanel123"
+
 );
 
+
+
 $db->setAttribute(
+
 PDO::ATTR_ERRMODE,
+
 PDO::ERRMODE_EXCEPTION
+
 );
+
 
 ?>
