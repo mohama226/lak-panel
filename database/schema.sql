a8827f5e54edc9fef27644ff60a6c5527f52ea93
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-23 20:31:55 +0330

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -2,12 +2,42 @@ CREATE TABLE admins (
 
 id INT AUTO_INCREMENT PRIMARY KEY,
 
-username VARCHAR(50) UNIQUE,
+username VARCHAR(50) UNIQUE NOT NULL,
 
-password VARCHAR(255),
+password VARCHAR(255) NOT NULL,
 
-role VARCHAR(50),
+role VARCHAR(50) DEFAULT 'admin',
 
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+);
+
+
+
+CREATE TABLE users (
+
+id INT AUTO_INCREMENT PRIMARY KEY,
+
+
+username VARCHAR(100) UNIQUE NOT NULL,
+
+
+password VARCHAR(255) NOT NULL,
+
+
+status ENUM('active','blocked') DEFAULT 'active',
+
+
+expire_date DATE NULL,
+
+
+total_gb INT DEFAULT 0,
+
+
+used_gb INT DEFAULT 0,
+
+
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
 
 );
