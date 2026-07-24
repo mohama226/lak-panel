42d01f9dda96e17528c6cb625557ba9e14b287f8
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 14:30:54 +0330

    Update header.php

--- public/includes/header.php
+++ public/includes/header.php
@@ -16,6 +16,8 @@ L-PANEL | مدیریت VPN
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<link href="https://cdn.jsdelivr.net/npm/vazirmatn@latest/misc/UI-Farsi-Digits/Vazirmatn-UI-FD-font-face.css" rel="stylesheet">
+
 <link rel="stylesheet" href="/assets/css/style.css">
 <link rel="stylesheet" href="/assets/css/dropdown.css">
 <link rel="stylesheet" href="/assets/css/users.css">
@@ -26,4 +28,3 @@ L-PANEL | مدیریت VPN
 </head>
 
 <body>
-
