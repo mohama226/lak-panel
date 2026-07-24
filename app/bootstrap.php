ab25a9e76ac3fee814e996690a6456ee1df65a73
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 19:03:01 +0330

    Create bootstrap.php

---
+++ app/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+define(
+    'BASE_PATH',
+    dirname(__DIR__)
+);
+
+
+require_once BASE_PATH.'/app/session.php';
+require_once BASE_PATH.'/app/database.php';
+require_once BASE_PATH.'/app/logger.php';
+require_once BASE_PATH.'/app/auth.php';
