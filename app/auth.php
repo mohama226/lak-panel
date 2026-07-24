117e614889d652f4d05d14974f0ec9a9ffd9ca05
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 14:00:32 +0330

    Update auth.php

--- app/auth.php
+++ app/auth.php
@@ -1,29 +1,32 @@
 <?php
 
-
 if(session_status() === PHP_SESSION_NONE){
 
-session_start();
+    session_set_cookie_params([
+        'path' => '/',
+        'httponly' => true,
+        'samesite' => 'Lax'
+    ]);
+
+    session_start();
 
 }
-
 
 
 function checkLogin(){
 
 
-if(!isset($_SESSION['admin'])){
+    if(!isset($_SESSION['admin'])){
 
 
-header("Location: /modiran");
+        header("Location: /modiran/index.php");
 
-exit;
+        exit;
+
+
+    }
 
 
 }
-
-
-}
-
 
 ?>
