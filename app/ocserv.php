9baada435bd9f3058f116414f9b5b3a231eefd2e
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 11:58:21 +0330

    Update ocserv.php

--- app/ocserv.php
+++ app/ocserv.php
@@ -4,17 +4,16 @@
 function ocserv_add_user($username,$password)
 {
 
-
-$cmd="sudo /var/www/html/l-panel/scripts/ocserv-manager.sh add "
+$cmd =
+"sudo /var/www/html/l-panel/scripts/ocserv-manager.sh add "
 .escapeshellarg($username)." "
 .escapeshellarg($password);
 
 
-exec($cmd,$output,$result);
+exec($cmd,$out,$code);
 
 
-return $result===0;
-
+return $code===0;
 
 }
 
@@ -24,18 +23,37 @@ function ocserv_delete_user($username)
 {
 
 
-$cmd="sudo /var/www/html/l-panel/scripts/ocserv-manager.sh delete "
+$cmd =
+"sudo /var/www/html/l-panel/scripts/ocserv-manager.sh delete "
 .escapeshellarg($username);
 
 
-exec($cmd,$output,$result);
+
+exec($cmd,$out,$code);
 
 
-return $result===0;
+return $code===0;
 
 
 }
 
 
 
-?>
+function ocserv_change_password($username,$password)
+{
+
+
+$cmd =
+"sudo /var/www/html/l-panel/scripts/ocserv-manager.sh password "
+.escapeshellarg($username)." "
+.escapeshellarg($password);
+
+
+
+exec($cmd,$out,$code);
+
+
+return $code===0;
+
+
+}
