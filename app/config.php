11e39fc6688a9438fdc7b24b98d8d83c03272ffc
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 18:41:18 +0330

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,13 +1,11 @@
 <?php
 
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors',1);
 
 define("DB_HOST","localhost");
 define("DB_NAME","lpanel");
 define("DB_USER","lpanel");
 define("DB_PASS","lpanel123");
-
-session_start();
 
 ?>
