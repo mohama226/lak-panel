ab01e7b7d87c80a16d5ec85c5f01d4f94c85ada9
mohama226 <133166987+mohama226@users.noreply.github.com>
2026-07-24 13:09:10 +0330

    Update delete.php

--- public/modiran/users/delete.php
+++ public/modiran/users/delete.php
@@ -1,63 +1,36 @@
 <?php
 
-
 require "../../../app/auth.php";
-
 checkLogin();
 
-
 require "../../../app/database.php";
-
-
 require "../../../app/ocserv.php";
 
+$id = $_GET['id'];
 
-
-$id=$_GET['id'];
-
-
-
-$stmt=$db->prepare(
-
-"SELECT username FROM users WHERE id=?"
-
+$stmt = $db->prepare(
+    "SELECT username FROM users WHERE id=?"
 );
-
 
 $stmt->execute([$id]);
 
-
-$user=$stmt->fetch();
-
-
+$user = $stmt->fetch();
 
 if($user){
 
+    // 🔥 تغییر داده شد
+    ocserv_delete_user(
+        $user['username']
+    );
 
-ocserv_user_delete(
-$user['username']
-);
+    $stmt = $db->prepare(
+        "DELETE FROM users WHERE id=?"
+    );
 
-
-
-$stmt=$db->prepare(
-
-"DELETE FROM users WHERE id=?"
-
-);
-
-
-$stmt->execute([$id]);
-
-
+    $stmt->execute([$id]);
 }
 
-
-
 header("Location:/modiran/users");
-
-
 exit;
-
 
 ?>
